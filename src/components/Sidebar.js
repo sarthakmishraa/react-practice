629388b29587f46b7a1aae446f12eb9d96975c9b
Sarthak Mishra <msarthak785@gmail.com>
2024-04-08 22:37:58 -0400

    fixed sidebar dropdown

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,8 +5,8 @@ function Sidebar() {
   const [showButton, setShowButton] = useState(false);
   
   return (
-    <div className="h-100% w-160px fixed z-1 left-0 p-5 bg-gradient-to-t from-sky-400 to-sky-100 rounded-b-xl shadow-xl">
-      <button className="active:scale-75 transition duration-50 text-2xl font-bold text-white py-2 px-3 bg-sky-500 hover:bg-sky-800 text-3xl rounded-md shadow-xl my-2" onClick={() => setShowButton(!showButton)}>☰ Menu</button>
+    <div className="h-100% w-160px fixed z-1 left-0 p-5 bg-gradient-to-t from-sky-400 to-sky-100 rounded-b-xl shadow-xl" onMouseEnter={() => {setShowButton(true)}} onMouseLeave={() => {setShowButton(false)}}>
+      <button className="active:scale-75 transition duration-50 text-2xl font-bold text-white py-2 px-3 bg-sky-500 hover:bg-sky-800 text-3xl rounded-md shadow-xl my-2">☰ Menu</button>
       {showButton &&
         <div className="mt-5">
           <li className='list-none mb-3 font-bold text-slate-300 py-2 px-3 bg-sky-800 hover:bg-sky-600 text-xl rounded-md shadow-xl'><Link to="/profile">Profile</Link></li>
